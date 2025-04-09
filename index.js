157fc177958c44cf62c42a6ff5b77e53ded74c11
Shreyas-devLover <shreyaskumar103+git@gmail.com>
2025-04-09 14:57:28 +0530

    InitialCommit

---
+++ index.js
@@ -0,0 +1,5 @@
+function next() {
+    document.getElementById("MobileView").style.display = 'none'
+    document.getElementById("DesktopView").style.display = 'none'
+    document.getElementById("LoginPage").style.display = "block"
+}
