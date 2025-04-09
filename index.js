ba6db2d29c89a863ae83a535ddd928d0a9c0ad95
JakeTalo <140406613+JakeTalo@users.noreply.github.com>
2025-04-09 15:18:14 +0530

    Login Page has been seperated

--- index.js
+++
@@ -1,5 +0,0 @@
-function next() {
-    document.getElementById("MobileView").style.display = 'none'
-    document.getElementById("DesktopView").style.display = 'none'
-    document.getElementById("LoginPage").style.display = "block"
-}
